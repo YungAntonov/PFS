51c2460f5f9d4c71670a4026c45fe804b5b56514
Antoine Bonnat <antonov@antoines-macbook-pro.home>
2022-10-29 16:15:33 +0200

    création MenuItems

---
+++ src/components /Navbar/MenuItems.js
@@ -0,0 +1,8 @@
+const MenuItems = [
+    {
+        title : "Home" ,
+        url :"#" ,
+
+    }
+
+]
---
+++ src/components /Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+
+    render () {
+        return (
+            <nav className = "NavbarItems">
+                <h1 className="navbarlogo"> zizi </h1>
+                <div className="menu-icon">
+
+
+                </div>
+                <ul>
+                    <li> <a href=""> </a> </li>
+                </ul>
+            </nav>
+        )
+
+    }
+
+}
