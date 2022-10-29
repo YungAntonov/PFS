47ccf13f272a85cd0dd23fec81b8cdf2e4fa72ef
Antoine Bonnat <antonov@antoines-macbook-pro.home>
2022-10-29 15:51:26 +0200

    Création components navbar

---
+++ pfs/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='app'> 
+    <p>Coucou Océ :^)
+    </p>
+    
+    </div>
+  );
+}
+
+export default App;
