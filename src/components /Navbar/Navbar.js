30228ed707b78c3d9bb077c916a21054b91d0632
Antoine Bonnat <antonov@antoines-macbook-pro.home>
2022-10-30 10:48:38 +0100

    add StyleSheet CDN

--- src/components /Navbar/Navbar.js
+++ src/components /Navbar/Navbar.js
@@ -23,7 +23,6 @@ class Navbar extends Component {
                             </li>
                             
                         )
-
                     })}
                     
                     
